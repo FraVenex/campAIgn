aead9db2fe4f32f45727afdea619cfe21e6e37dd
Francesco Veneziano <fveneziano@alteanet.it>
2026-05-22 16:44:03 +0200

    chore: track environment.ts and remove from gitignore to fix build in CI
    Co-Authored-By: Antigravity <antigravity@google.com>

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://rlixpechojwaxwazbxcq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsaXhwZWNob2p3YXh3YXpieGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3Nzg3NTQsImV4cCI6MjA5MjM1NDc1NH0.HZmDXlzgeAyNXic5iO7paN98ffxjUAsRfPk8yjO8CZQ'
+};
